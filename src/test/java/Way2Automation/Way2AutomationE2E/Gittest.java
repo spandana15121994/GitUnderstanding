ec940c0403da612fe1b0f06a7574de889a028cdd
26IN <tospandana1234@gmail.com>
2020-05-30 17:23:02 +0530

    changes made by person Y

---
+++ src/test/java/Way2Automation/Way2AutomationE2E/Gittest.java
@@ -0,0 +1,15 @@
+package Way2Automation.Way2AutomationE2E;
+
+import org.testng.annotations.Test;
+
+public class Gittest {
+	
+	@Test
+	public void selenium()
+	{
+		System.out.println("web automation");
+		System.out.println("mobile automation");
+		System.out.println("git understanding");
+	}
+
+}
